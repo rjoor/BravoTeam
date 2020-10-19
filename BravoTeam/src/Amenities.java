957572eb83bd646b0469e601f91f44511ebc3aa3
ridgetj <70304157+ridgetj@users.noreply.github.com>
2020-10-19 14:28:21 -0400

    Completed the enums

---
+++ BravoTeam/src/Amenities.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ * @author Ridge Johnson
+ * The Amenities enum displays the different amenities that are provided with each listing.
+ */
+public enum Amenities {
+	Pool, PetFriendly, Parking, ShuttleServices, Internet, Dishwasher, InUnitLaundry
+}
---
+++ BravoTeam/src/Type.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ * @author Ridge Johnson
+ * The Type enum displays the different types of the listings listed.
+ */
+public enum Type {
+	House, Apartment, Townhouse, Duplex
+}
