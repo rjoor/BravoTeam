0f185d4cfcd9ca7bf34b90a346ea6faa39d82c8c
vancevince <kolblugo@email.sc.edu>
2020-10-23 07:37:38 -0400

    Adding StudentConstants

---
+++ BravoTeam/src/StudentConstants.java
@@ -0,0 +1,22 @@
+/**
+ * StudentConstants class keeps track of attributes/fields that make up a JSON file for 
+ * a student user. 
+ * 
+ * TODO review field variable types. Should Favorites List be an arraylist?
+ * Is Guest a boolean? Is USC Student a boolean? 
+ * 
+ * @author Vince Kolb-Lugo
+ * Bravo Team
+ * 2020-10-23
+ *
+ */
+public abstract class StudentConstants {
+	protected static final String STUDENT_FILE_NAME = "src/student.json";
+	protected static final String STUDENTS = "students";
+	protected static final String STUDENT_FIRST_NAME = "firstName";
+	protected static final String STUDENT_LAST_NAME = "lastName";
+	protected static final String IS_GUEST = "isGuest";
+	protected static final String STUDENT_ID = "studentid";
+	protected static final String FAVORITE_LISTINGS = "favoritesListings";
+	protected static final String IS_USC_STUDENT = "isUSCStudent";
+}
