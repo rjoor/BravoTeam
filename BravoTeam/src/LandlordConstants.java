6a570a5af8ef17100b92e2bc4f6dd5b324d1dac7
vancevince <kolblugo@email.sc.edu>
2020-10-23 08:02:16 -0400

    Adding LandlordsConstants

---
+++ BravoTeam/src/LandlordConstants.java
@@ -0,0 +1,17 @@
+/**
+ * LandlordConstants class keeps track of the attributes/fields that make up
+ * a JSON file for a landlord.
+ * 
+ * @author Vince Kolb-Lugo
+ * Bravo Team
+ * 2020-10-23
+ *
+ */
+public abstract class LandlordConstants {
+	protected static final String ID = "id";
+	protected static final String FIRST_NAME = "firstName";
+	protected static final String LAST_NAME = "lastName";
+	protected static final String IS_GUEST = "isGuest";
+	protected static final String RATING = "rating";
+	protected static final String PROPERTIES_MANAGED = "propertiesManaged";
+}
