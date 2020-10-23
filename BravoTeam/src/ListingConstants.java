17bce8186b7db957df534aacabf79d52e8dfd587
vancevince <kolblugo@email.sc.edu>
2020-10-23 08:20:28 -0400

    Adding ListingConstants class

---
+++ BravoTeam/src/ListingConstants.java
@@ -0,0 +1,23 @@
+/**
+ * ListingConstants keeps track of the attributes/fields that make up a 
+ * Listing JSON file. 
+ * 
+ * @author Vince Kolb-Lugo
+ * Bravo Team
+ * 2020-10-23
+ *
+ */
+public abstract class ListingConstants {
+	protected static final String ID = "id";
+	protected static final String ADDRESS = "address";
+	protected static final String NUM_BEDS = "numBeds";
+	protected static final String SQUARE_FOOTAGE = "squareFootage";
+	protected static final String AMENITIES = "amenities";
+	protected static final String RENT_COST = "rentCost";
+	protected static final String UTILITIES_INCLUDED = "utilitiesIncluded";
+	protected static final String LEASE_DURATION = "leaseDuration";
+	protected static final String CAN_SUBLET = "canSublet";
+	protected static final String DISTANCE_FROM_CAMPUS = "distanceFromCampus";
+	protected static final String IS_HANDICAP_ACCESSIBLE = "isHandicapAccessible";
+	protected static final String TYPE = "type";
+}
