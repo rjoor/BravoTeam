a173080ab7c9b3b3e8349c8d4ecce61aa2599167
ridgetj <70304157+ridgetj@users.noreply.github.com>
2020-10-26 15:07:13 -0400

    Incomplete ListingWriter

--- BravoTeam/src/ListingWriter.java
+++ BravoTeam/src/ListingWriter.java
@@ -1,4 +1,32 @@
+/**
+ * 
+ * @author ridgetj Bravo Team 10/25/20
+ *
+ */
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
-public class ListingWriter {
-
+public class ListingWriter extends ListingConstants {
+	//TODO
+	
+public static JSONObject getListingJSON(Listing listing) {
+		JSONObject listingDetails = new JSONObject();
+		listingDetails.put(ID, listing.getID());	
+		listingDetails.put(Address, listing.getAddress());	
+		listingDetails.put(NumBeds, listing.getNumBeds());	
+		listingDetails.put(SquareFootage, listing.getSquareFootage());	
+		listingDetails.put(Amenities, listing.getAmenities());	
+		listingDetails.put(RentCost, listing.getID());	
+		listingDetails.put(UtilitiesIncluded, listing.getUtilitiesIncluded());	
+		listingDetails.put(LeaseDuration, listing.getLeaseDuration());	
+		listingDetails.put(CanSublet, listing.getCanSublet());	
+		listingDetails.put(DistanceFromCampus, listing.getDistanceFromCampus());	
+		listingDetails.put(IsHandicapAccessible, listing.getIsHandicapAccessible());	
+		listingDetails.put(Type, listing.getType());	
+		
+		return listingDetails;
+	}
 }
