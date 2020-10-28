71b3405448a31323eba7a726c467bd8cc6bfc759
vancevince <kolblugo@email.sc.edu>
2020-10-28 08:47:42 -0400

    Added LeaseConstants class

---
+++ BravoTeam/src/LeaseConstants.java
@@ -0,0 +1,20 @@
+/**
+ * LeaseConstants super class contains all the data fields needed complete 
+ * a lease agreement between a tenant and landlord. 
+ * 
+ * @author Vince Kolb-Lugo
+ * Bravo Team
+ * 2020-10-28
+ *
+ */
+public abstract class LeaseConstants {
+	protected static final String DATE = "date";
+	protected static final String LANDLORD = "landlord";
+	protected static final String TENANTS = "tenants";
+	protected static final String NUM_BED = "num_bed";
+	protected static final String START_DATE = "start date";
+	protected static final String END_DATE = "end date";
+	protected static final String RENT = "rent";
+	protected static final String PAYMENT_ADDRESS = "payment address";
+	protected static final String DAMAGE_COST = "damage cost";
+}
