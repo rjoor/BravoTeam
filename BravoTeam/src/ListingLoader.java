9046ee42b50726fd48fd23938acc27b4ae87ea90
ridgetj <70304157+ridgetj@users.noreply.github.com>
2020-10-29 01:06:36 -0400

    Added to ListingLoader

--- BravoTeam/src/ListingLoader.java
+++ BravoTeam/src/ListingLoader.java
@@ -14,28 +14,27 @@ public class ListingLoader extends ListingConstants {
 		ArrayList<Listing> listings = new ArrayList<Listing>();
 		
 		try {
-			FileReader reader = new FileReader(LISTING_FILE_NAME);
+			FileReader reader = new FileReader(LISTING);
 			JSONParser parser = new JSONParser();
-			JSONArray listingsJSON = (JSONArray) new JSONParser().parse(reader);
+			JSONArray landlordJSON = (JSONArray) new JSONParser().parse(reader);
 			
-			for(int i = 0; i < listingsJSON.size(); i++)
+			for(int i - 0; i < listingJSON.size(); i++)
 			{
-				JSONObject listingJSON = (JSONObject)listingsJSON.get(i);
+				JSONObject listingJSON = (JSONObject)listingJSON.get(i);
 				int id = (int)listingJSON.get(ID);
 				String address = (String)listingJSON.get(ADDRESS);
 				int numBeds = (int)listingJSON.get(NUM_BEDS);
-				double numBaths = (double)listingJSON.get(NUM_BATHS);
 				int squareFootage = (int)listingJSON.get(SQUARE_FOOTAGE);
-				ArrayList<String> amenities = (ArrayList<String>)listingJSON.get(AMENITIES);
+				String amenities = (String)listingJSON.get(AMENITIES);
 				int rentCost = (int)listingJSON.get(RENT_COST);
-				boolean utilitiesIncluded =  (boolean)listingJSON.get(UTILITIES_INCLUDED);
+				String utilitiesIncluded =  (String)listingJSON.get(UTILITIES_INCLUDED);
 				String leaseDuration = (String)listingJSON.get(LEASE_DURATION);
 				boolean isCanSublet = (boolean)listingJSON.get(CAN_SUBLET);
 				int distanceFromCampus = (int)listingJSON.get(DISTANCE_FROM_CAMPUS);
 				boolean isHandicapAccessible = (boolean)listingJSON.get(IS_HANDICAP_ACCESSIBLE);
-				Type type = (Type)listingJSON.get(TYPE);
+				String type = (String)listingJSON.get(TYPE);
 				
-				listings.add(new Listing(address, numBeds, numBaths, squareFootage, amenities, rentCost, utilitiesIncluded, leaseDuration, isCanSublet, distanceFromCampus, isHandicapAccessible, id, type));
+				listings.add(new Listing(id, address, numBeds, squareFootage, amenities, rentCost, utilitiesIncluded, leaseDuration, isCanSublet, distanceFromCampus, isHandicapAccessible, type));
 			}
 			
 			return listings;
