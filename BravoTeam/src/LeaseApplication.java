b1bb5cc3f3440e04f174137865b6fbab6e36b8eb
vancevince <kolblugo@email.sc.edu>
2020-10-29 13:02:54 -0400

    Added Scanner and methods to LeaseApplication

--- BravoTeam/src/LeaseApplication.java
+++ BravoTeam/src/LeaseApplication.java
@@ -1,3 +1,10 @@
+package BravoTeam.src;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 /**
  * LeaseApplication class creates a simple lease application between a student and a landlord. Also
  * sends the completed application to the landlord.
@@ -5,10 +12,69 @@
  * @author Vince Kolb-Lugo Bravo Team 2020-10-18
  *
  */
-public class LeaseApplication {
+public class LeaseApplication extends LeaseConstants {
+	//TODO change variable types for instances to match the types in the user classes
+	private static Scanner input = new Scanner(System.in);
+	public String lease ="";
+	public Landlord landlord;
+	public Listing listing;
+	private String date, landlord_name, num_bed, num_bath, address, zip, start_date, end_date, rent, payment_address, damage_cost;
+	private ArrayList<String> tenants = new ArrayList<String>();
+	
+	
+	/**
+	 * getInfo method grabs information from the user and listing classes to populate
+	 * the fields of the lease agreement. 
+	 */
+	public void getInfo() {
+		date = this.date;
+		landlord_name = this.landlord.getFirstName();
+		tenants = this.tenants;
+		num_bed = Integer.parseInt(listing.getNumBeds());
+		num_bath = Double.parseDouble(listing.getNumBaths());
+		address = listing.getAddress();
+	}
+	
+	/**
+	 * makeTemplate method reads in the LeaseTemplate text file and returns
+	 * @param fileName
+	 */
+	public String makeTemplate(String fileName) {
+		try
+		{
+			File file = new File(fileName);
+			Scanner scannerObject = new Scanner(file);
+			
+			while(scannerObject.hasNextLine()) {
+				lease += scannerObject.nextLine();
+			}
+			return lease;
+			
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void parseTemplate(String lease) {
+		//TODO read the lease string, parse it for the constants, and 
+		//replace values with user and listing types
+	}
 
-  private User leasee;
-  private Listing residence;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+  //private User leasee;
+  //private Listing residence;
 
   /**
    * generateApp method passes a user, landlord, and address as parameters and generates a simple
@@ -18,16 +84,17 @@ public class LeaseApplication {
    * @param leaser a User of type landlord
    * @param residence a Listing of type listing
    */
-  public void generateApp(User leasee, User leaser, Listing residence) {
+  //public void generateApp(User leasee, User leaser, Listing residence) {
     // TODO set values here
-  }
+  //}
 
   /**
    * sendApp method sends the completed application to a landlord's email address
    * 
    * @param email a String of the landlord's email address
    */
-  public void sendApp(String email) {
+  //public void sendApp(String email) {
     // TODO write the logic here
-  }
+  //}
+  
 }
