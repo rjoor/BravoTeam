92da78623a33533ad670c05747c4ebf57a154769
Harith Kumte <hkumte@email.sc.edu>
2020-10-29 22:41:59 -0400

    Somewhat working Lease Application generator and editor

---
+++ BravoTeam/src/LeaseTester.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class LeaseTester
+{   
+    static void modifyFile(String filePath, String oldString, String newString)
+    {
+        File fileToBeModified = new File(filePath);
+         
+        String oldContent = "";
+         
+        BufferedReader reader = null;
+         
+        FileWriter writer = null;
+         
+        try
+        {
+            reader = new BufferedReader(new FileReader(fileToBeModified));
+             
+            //Reading all the lines of input text file into oldContent
+             
+            String line = reader.readLine();
+             
+            while (line != null) 
+            {
+                oldContent = oldContent + line + System.lineSeparator();
+                 
+                line = reader.readLine();
+            }
+             
+            //Replacing oldString with newString in the oldContent
+             
+            String newContent = oldContent.replaceAll(oldString, newString);
+             
+            //Rewriting the input text file with newContent
+             
+            writer = new FileWriter(fileToBeModified);
+             
+            writer.write(newContent);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                //Closing the resources
+                 
+                reader.close();
+                 
+                writer.close();
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+     
+    public static void main(String[] args) throws IOException
+    {
+        modifyFile("src/LeaseTemplate.txt", "<DATE>", "10/29/20");
+         
+        Scanner input = new Scanner(new File("src/LeaseTemplate.txt"));
+
+        while (input.hasNextLine())
+        {
+           System.out.println(input.nextLine());
+        }
+        System.out.println("done");
+    }
+}
